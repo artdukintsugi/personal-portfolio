b991eaf4ec6cf6073060fc2aace9f9c5b2def492
Evelyn Vybíralů <jonas.vybiral.jv@gmail.com>
2025-03-19 18:17:30 +0100

    added evelyn mathfrak kill me please

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/NavBar";
+import Evelyn from "@/components/mathfrak/Evelyn";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       bing bong jsem na tohle leva xd ale aspon mame ikonku
+      <Evelyn />
     </>
   );
 }
---
+++ components/mathfrak/Evelyn.jsx
@@ -0,0 +1,9 @@
+import Latex from "react-latex-next";
+
+export default function Evelyn() {
+  return (
+    <Latex>
+      $\mathfrak e\mathfrak v\mathfrak e\mathfrak l\mathfrak y\mathfrak n$
+    </Latex>
+  );
+}
