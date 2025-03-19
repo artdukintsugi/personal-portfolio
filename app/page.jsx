824184520615c5e45554e32b9c4257ddad60e2da
Evelyn Vybíralů <jonas.vybiral.jv@gmail.com>
2025-03-19 17:41:05 +0100

    added icon

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      bing bong jsem na tohle leva xd ale aspon mame ikonku
+    </>
+  );
+}
